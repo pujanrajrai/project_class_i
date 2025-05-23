e8e077ee174ecee338a729d232e797641c61e0b3
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2025-05-23 09:00:19 +0545

    file structure updated

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-def add_student():
-    roll_no = input("Enter Your Roll No: ")
-    name = input("Enter Your Name: ")
-    contact_no = input("Enter Your Contact No: ")
-    with open("student.txt", "a") as student:
-        student.write(f"{roll_no},{name},{contact_no}\n")
-    print("Student Record Added Successfully")
-
-
-def view_student():
-    with open("student.txt", "r") as student_data:
-        students = student_data.readlines()
-        for student in students:
-            print(student.strip())
-
-
-def update_student():
-    print("You are on Update Student Function")
-
-
-def delete_student():
-    print("You are on Delete Student Function")
+from student_management import (
+    add_student, update_student,
+    view_student, delete_student
+)
 
 
 def menu():
